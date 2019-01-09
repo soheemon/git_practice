eb454298a4009ff98aabfa78cf05309f8dc6fc0d
정철희 <wjdcjf1229@gmail.com>
2019-01-09 11:51:53 +0900

    sub2 sum

--- Project/src/kosta/HelloWorld.java
+++ Project/src/kosta/HelloWorld.java
@@ -6,7 +6,11 @@ public class HelloWorld {
 		//인코딩좀해줘어엉
 		System.out.println("hello!");
 		System.out.println("sub2브랜치에서 작업");
-
+		int sum =0;
+		for(int i=1;i<=100;i++) {
+			sum+=i;
+		}
+		System.out.println(sum);
 	}
 
 }
