64327e81c50bea64b58d6c95bcc8f1486aa6dce8
soheemon <2170210@gmail.com>
2019-01-09 10:59:00 +0900

    merge

--- Project/src/kosta/HelloWorld.java
+++ Project/src/kosta/HelloWorld.java
@@ -3,12 +3,9 @@ package kosta;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		//Ãæµ¹¹ß»ý ¿¹Á¦
 		System.out.println("hello!");
-=======
-		System.out.println("ì†Œížˆëˆ„ë‚˜ëŠ” ë¹›ê³¼ì†Œê¸ˆ");
->>>>>>> branch 'master' of https://github.com/soheemon/git_practice.git
+		System.out.println("?†Œ?žˆ?ˆ„?‚˜?Š” ë¹›ê³¼?†Œê¸?");
 
 	}
 
